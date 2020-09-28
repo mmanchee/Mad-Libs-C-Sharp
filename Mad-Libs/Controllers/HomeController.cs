4f03ccfaace81b4aeea9a1e327c2d2bb85ed531e
Garrett <gman9mm@live.com>
2020-09-28 10:57:10 -0700

    Add mad lib structuring, forms, etc.
    Co-authored-by: Mike Manchee <mikemanchee@gmail.com>
    Co-authored-by: Ben Stoller <stollersystemsound@gmail.com>

--- Mad-Libs/Controllers/HomeController.cs
+++ Mad-Libs/Controllers/HomeController.cs
@@ -1,15 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
-using FriendLetter.Models;
+using MadLibs.Models;
 
 namespace MadLibs.Controllers
 {
   public class HomeController : Controller
   {
-    [Route("/form")]
-    public ActionResult Form() { return View(); }
+    // [Route("/form")]
+    // public ActionResult Form() { return View(); }
 
     [Route("/")]
     public ActionResult Form() { return View(); }
+
+    [Route("/madlib")]
+    public ActionResult MadLib(string noun, string exclamation, string verb, string name, string dog, string monster)
+    {
+      WordVariable myWordVariable = new WordVariable();
+      myWordVariable.Exclamation = exclamation;
+      myWordVariable.Verb = verb;
+      myWordVariable.Name = name;
+      myWordVariable.Dog = dog;
+      myWordVariable.Noun = noun;
+      myWordVariable.Monster = monster;
+      return View(myWordVariable);
+    }
+
 
 
   }
--- Mad-Libs/Models/WordVariable.cs
+++ Mad-Libs/Models/WordVariable.cs
@@ -2,8 +2,13 @@ namespace MadLibs.Models
 {
   public class WordVariable
   {
-    public string Input { get; set; }
-    public string input { get; set; }
+    public string Noun { get; set; }
+    public string Exclamation { get; set; }
+    public string Verb { get; set; }
+    public string Name { get; set; }
+    public string Dog { get; set; }
+    public string Monster { get; set; }
+
 
   }
 }
